e7483b0ac3b1be4c5cb148d8680e428b59244b7f
Chris Kabana <chris.kabana@gmail.com>
2020-09-28 21:12:44 -0500

    setting up routers

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,60 @@
+//Dependencies
+//==============================================================================
+var express = require("express");
+
+var router = express.Router();
+
+//import the model (burger.js) to use its database functions
+var burger = require("../models/burger.js");
+
+//Create all routes and set up logic within those routes where required
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/api/burgers", function(req, res) {
+    burger.create([
+        "burger_name", "devoured"
+    ], [
+        req.body.burger_name, req.body.devoured
+    ], function(result) {
+        res.json({ id: result.insertId });
+    });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.update ({
+        devoured: req.body.devoured
+    }, condition, function(result) {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+router.delete("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    burger.delete(condition, function(result) {
+        if (result.affectedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+//Export routes for server.js to use
+module.exports = router;
