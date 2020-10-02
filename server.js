f05a305cfdd5e1a0454c467736f707ca8ea71aa7
Chris Kabana <chris.kabana@gmail.com>
2020-10-02 11:12:41 -0500

    setting up server.js

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 //DEPENDENCIES
 //============================================================
-var mysql = require("mysql");
+var express = require("express");
 
-var connection = mysql.createConnection({
-    host: "localhost",
+var PORT = process.env.PORT || 8080;
 
-    port: 8080,
-})
+var app = express();
+
+//static content from the 'public' folder
+app.use(express.static("public"));
+
+//parse application body as json
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//set up Handlebars
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//import routes and give the server access to them
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+//start the server so that it can begin listening to client requests
+app.listen(PORT, function() {
+    //log server side when the server has started
+    console.log("Server listening on: " + PORT);
+});
